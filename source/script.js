de5ee7a5fb5061ba0885ff0c683d7b4b425c38d1
sasha-ak <157721905+sasha-ak@users.noreply.github.com>
2024-04-23 18:20:09 +0200

    Added AI API integration, teached AI to provide required information, added Axios library

--- source/script.js
+++ source/script.js
@@ -1,12 +1,27 @@
+function displayMotivationalQuote(response) {
+  new Typewriter("#quote", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+  });
+}
+
 function generateNewQuote(event) {
   event.preventDefault();
-  new Typewriter("#quote", {
-    strings: "All you need is Motivation!",
-    autoStart: true,
-    //cursor: "",
-    //delay: 1,
-  });
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "b2t5aaa2b0903bc42e03fo6f63f58de7";
+  let context =
+    "You are a motivational expert, you provide inspirational quotes to motivate people, you love to write short quotes to help people to feel better. Your mission is to generate 1-2 sentences quote in basic HTML and separate each sentence with </br>. Make sure to follow the user instructions. Sign the quote with `SheCodes AI` using <strong></strong> element. Do not use <strong></strong> element for the quote. Do not use doublequotes for each quote.";
+
+  let prompt = `User instructions: generate inspirational quote about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  //make a call to API
+
+  axios.get(apiUrl).then(displayMotivationalQuote);
 }
 
 let quoteButton = document.querySelector("#quote-generator-form");
 quoteButton.addEventListener("submit", generateNewQuote);
+let quoteForm = document.querySelector("#quote");
+quoteForm.innerHTML = "How do you feel today?";
