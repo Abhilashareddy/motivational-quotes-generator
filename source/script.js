e1e8622746521cf0281baa91842721e2ed12807d
sasha-ak <157721905+sasha-ak@users.noreply.github.com>
2024-04-23 14:55:23 +0200

    Added basic JS form handling. Added Typewriter plugin

--- source/script.js
+++ source/script.js
@@ -1,1 +1,12 @@
+function generateNewQuote(event) {
+  event.preventDefault();
+  new Typewriter("#quote", {
+    strings: "All you need is Motivation!",
+    autoStart: true,
+    //cursor: "",
+    //delay: 1,
+  });
+}
 
+let quoteButton = document.querySelector("#quote-generator-form");
+quoteButton.addEventListener("submit", generateNewQuote);
